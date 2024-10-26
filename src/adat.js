de78782479c7fb1ba426de94691592533fa7e868
BotkaB <botikabinaka@gmail.com>
2024-10-26 09:50:27 +0200

    adat.js és képek mappa

---
+++ src/adat.js
@@ -0,0 +1,49 @@
+export const kepLista = [
+    {
+        url: "kepek/DSC_0051.JPG",
+        cim: "Szittyótér vége",
+        leiras: "Szelidi-tó főterének vége.",
+    },
+    {
+        url: "kepek/DSC_0076.JPG",
+        cim: "Forgalmi rend változás",
+        leiras: "Ráhajtogatták a táblát az oszlopra.",
+    },
+
+    {
+        url: "kepek/DSC_0078.JPG",
+        cim: "Vallomás a Jászságról",
+        leiras: "Jásztalálkozón jász identitás."
+    },
+    {
+        url: "kepek/DSC_0092.JPG",
+        cim: "Tiltó tábla",
+        leiras: "Kis kiegészítéssel már nem tiltó.",
+    },
+    {
+        url: "kepek/DSC_0162.JPG",
+        cim: "Sportpályák vendégeknek",
+        leiras: "Motiváció sportra vagy ivásra."
+    },
+    {
+        url: "kepek/DSC_0181.JPG",
+        cim: "Csikkek helye",
+        leiras: "Papírlap a földön csikk helyett.",
+    },
+    {
+        url: "kepek/DSC_0210.JPG",
+        cim: "Én vagyok az a pád",
+        leiras: "csak az anya a biztos.",
+
+    },
+    {
+        url: "kepek/DSC_0329.JPG",
+        cim: "Megfejthetetlen",
+        leiras: "olvashatatlan",
+    },
+    {
+        url: "kepek/DSC_0541.JPG",
+        cim: "Ne etesd a kacsákat",
+        leiras: "És fürdeni is tilos!",
+    },
+];
